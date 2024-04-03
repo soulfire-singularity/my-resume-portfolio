0f062a85fcea79e202e5ef2116b41d2a7cd68992
Ethan <81447958+soulfire-singularity@users.noreply.github.com>
2024-04-03 11:48:23 -0500

    created my resume

---
+++ js/resume.js
@@ -0,0 +1,7 @@
+let firstName = "Ethan ";
+let midInitial = "A";
+let lastName = "Norman ";
+
+let fullName = firstName + midInitial + ". " + lastName;
+
+document.getElementById("profile-name").innerHTML = fullName;
